deb0eac9477342263b4a882e542cab5060c410cf
Fitrah-16520207 <fitnug30@gmail.com>
2022-03-13 20:47:48 +0700

    Update Program.cs

--- DFS/ConsoleApp1/Program.cs
+++ DFS/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
