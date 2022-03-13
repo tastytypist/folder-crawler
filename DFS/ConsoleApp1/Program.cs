718293f03dc471547a6f8fdb1fe1a4f9843d0118
Fitrah-16520207 <fitnug30@gmail.com>
2022-03-13 22:07:48 +0700

    Update Program.cs

--- DFS/ConsoleApp1/Program.cs
+++ DFS/ConsoleApp1/Program.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 
-namespace HelloWorldApplication
+namespace RectangleApplication
 {
-    class HelloWorld
+    using System;
+    using System.IO;
+
+    class CopyDir
     {
-        static void Main(string[] args)
+        public static string searchFolder(DirectoryInfo source, string target)
         {
-            /* my first program in C# */
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            // Check if the target directory exists, if not, create it.
+
+            // Copy each file into it's new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(fi);
+                if (fi.Name == target)
+                {
+                    return fi.FullName;
+                }
+            }
+
+            // Copy each subdirectory using recursion.
+            string subdir = @"";
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    source.CreateSubdirectory(diSourceSubDir.Name);
+                string file = searchFolder(diSourceSubDir, target); 
+                if (file!= "")
+                {
+                    return file;
+                } 
+            }
+            return subdir;
         }
+
+        public static void Main()
+        {
+            string sourceDirectory = @"D:\Game";
+            string targetDirectory = @"Battlefield 1.rar";
+
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+        //    FileInfo diTarget = new FileInfo(targetDirectory);
+            
+            string cari = searchFolder(diSource,targetDirectory);
+            Console.WriteLine(cari);
+            FileInfo diTarget = new FileInfo(cari);
+            Console.ReadLine();
+        }
+
+        // Output will vary based on the contents of the source directory.
     }
 }
