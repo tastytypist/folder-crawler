69287aac9b1abfa89b28ad8a382bb4a1b7e63464
HanselTanoto <hansel.tanoto05@gmail.com>
2022-03-14 17:26:31 +0700

    Update MainWindow.xaml

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -24,5 +27,23 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+        private void btnChooseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var openFolderDialog = new FolderBrowserDialog();
+            openFolderDialog.ShowDialog();
+            //    txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        /*private void ShowOpenFolderDialog()
+        {
+            RadOpenFolderDialog openFolderDialog = new RadOpenFolderDialog();
+            openFolderDialog.Owner = this;
+            openFolderDialog.ShowDialog();
+            if (openFolderDialog.DialogResult == true)
+            {
+                string folderName = openFolderDialog.FileName;
+            }
+        }*/
     }
 }
