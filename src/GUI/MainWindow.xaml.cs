71acf47fe5df6c6be4803a49b78af9e669cb9bdf
HanselTanoto <hansel.tanoto05@gmail.com>
2022-03-15 10:22:27 +0700

    Update MainWindow.xaml.cs
    Input/Output handler

--- src/GUI/MainWindow.xaml.cs
+++ src/GUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -34,19 +35,51 @@ namespace GUI
         {
             var openFolderDialog = new FolderBrowserDialog();
             openFolderDialog.ShowDialog();
-            StartDirectory.Text = openFolderDialog.SelectedPath;
+            startDirectory.Text = openFolderDialog.SelectedPath;
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            clearOutputScreen();
 
+            string start = startDirectory.Text;                     // Nama strating directory
+            string fileName = ipFileName.Text;                      // Nama file yang ingin dicari
+            bool Occurence = (bool) ipFindAllOccurence.IsChecked;   // Mode pencarian (semua kemunculan (true) / kemunculan pertama (false))
+            
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            if (btnBFS.IsChecked == true)
+            {
+                // ALGORITMA BFS
+                
+            } 
+            else if (btnDFS.IsChecked == true)
+            {
+                // ALGORITMA DFS
+
+            }
+            stopWatch.Stop();
+
+            // Output
+            opPathList.Items.Add(@"C:\");   // Contoh, parameter string bisa diganti dengan directory hasil pencarian
+            opTimeSpent.Text += stopWatch.ElapsedMilliseconds.ToString() + " ms";
         }
 
         private void pathFile_Click(object sender, RoutedEventArgs e)
         {
             // Sementara
-            var appPath = System.AppDomain.CurrentDomain.BaseDirectory;
-            System.Diagnostics.Process.Start(appPath);
+            for (int i = 0; i < opPathList.Items.Count; i++)
+            {
+                //var appPath1 = System.AppDomain.CurrentDomain.BaseDirectory;
+                var appPath = opPathList.Items[i].ToString();
+                Process.Start(appPath);
+            }
+        }
+
+        private void clearOutputScreen()
+        {
+            opPathList.Items.Clear();
+            opTimeSpent.Text = "Time Spent: ";
         }
     }
 }
