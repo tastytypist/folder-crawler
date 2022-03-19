4d008f27ca6ec098d003feecd68aab84d15743cf
tastytypist <nathanael310802@gmail.com>
2022-03-19 22:04:08 +0700

    Add BFS namespace project and structure

---
+++ src/BFS/BFS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using DFS;
+
+namespace BFS;
+
+public class BreadthFirstSearch
+{
+    private bool _fileSearched;
+    private int _timeSpent;
+    private NTree<string> _searchTree;
+    private Queue<string> _searchQueue;
+    private string[] _filePaths;
+}
